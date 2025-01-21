f22e35bef360e872f03d9b8593aa56c06ac6de68
JLeVangi <47614776+JPLeVangie@users.noreply.github.com>
2025-01-21 18:56:07 -0500

    Initial commit of source

---
+++ app.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python3
+#
+# Fetch a Gadgetbridge database export from a local drive
+# and extract smart ring stats to write into InfluxDB.
+#
+import os
+import shutil
+import sqlite3
+import sys
+import tempfile
+import time
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+### Config section
+# Path to the local export file
+LOCAL_PATH = os.getenv("LOCAL_PATH")
+
+# How far back in time should we query when extracting stats?
+QUERY_DURATION = int(os.getenv("QUERY_DURATION", 86400))
+
+# InfluxDB settings
+INFLUXDB_URL = os.getenv("INFLUXDB_URL")
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
+INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "")
+INFLUXDB_MEASUREMENT = os.getenv("INFLUXDB_MEASUREMENT")
+INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET")
+
+# For testing/debugging only
+REMOVE_TEMP_DB = os.getenv("REMOVE_TEMP_DB", "Y")
+
+def fetch_database():
+    '''Fetch the database from the local path'''
+    if not os.path.exists(LOCAL_PATH):
+        print("Error: Export file does not exist")
+        sys.exit(1)
+        
+    tempdir = tempfile.mkdtemp()
+    shutil.copy(LOCAL_PATH, f'{tempdir}/gadgetbridge.sqlite')
+    return tempdir
+
+def open_database(tempdir):
+    '''Open a handle on the database'''
+    conn = sqlite3.connect(f"{tempdir}/gadgetbridge.sqlite")
+    cur = conn.cursor()
+    return conn, cur
+
+def extract_data(cur):
+    '''Query the database for smart ring data'''
+    results = []
+    devices = {}
+    devices_observed = {}
+    query_start_bound = int(time.time()) - QUERY_DURATION
+    # Some tables use ms timestamps
+    query_start_bound_ms = query_start_bound * 1000
+
+    # Pull out device names (focusing on Colmi ring)
+    device_query = "select _id, NAME from DEVICE where NAME LIKE '%Colmi%'"
+    try:
+        res = cur.execute(device_query)
+    except sqlite3.OperationalError as e:
+        print("Unable to fetch stats - received an empty database")
+        return False
+    
+    for r in res.fetchall():
+        devices[f"dev-{r[0]}"] = r[1]
+
+    if not devices:
+        print("No Colmi ring found in database")
+        return False
+
+    print("Devices found:", devices)
+
+    # Get stress level data
+    print("Querying stress level data...")
+    data_query = ("SELECT TIMESTAMP, DEVICE_ID, STRESS FROM COLMI_STRESS_SAMPLE "
+        f"WHERE TIMESTAMP >= {query_start_bound} "
+        "AND DEVICE_ID IN (" + ",".join([k.split('-')[1] for k in devices.keys()]) + ") "
+        "ORDER BY TIMESTAMP ASC")
+    
+    res = cur.execute(data_query)
+    for r in res.fetchall():
+        row_ts = r[0] * 1000000  # Convert to nanoseconds
+        row = {
+                "timestamp": row_ts,
+                "fields" : {
+                    "stress_level" : r[2]
+                },
+                "tags" : {
+                    "device" : devices[f"dev-{r[1]}"]
+                }
+        }
+        results.append(row)
+        if f"dev-{r[1]}" not in devices_observed or devices_observed[f"dev-{r[1]}"] < row_ts:
+            devices_observed[f"dev-{r[1]}"] = row_ts
+
+    print("Stress level data points:", len(results))
+
+    # Get sleep session data
+    print("Querying sleep session data...")
+    data_query = ("SELECT TIMESTAMP, DEVICE_ID, WAKEUP_TIME FROM COLMI_SLEEP_SESSION_SAMPLE "
+        f"WHERE TIMESTAMP >= {query_start_bound} "
+        "AND DEVICE_ID IN (" + ",".join([k.split('-')[1] for k in devices.keys()]) + ") "
+        "ORDER BY TIMESTAMP ASC")
+
+    res = cur.execute(data_query)
+    for r in res.fetchall():
+        row_ts = r[0] * 1000000  # Convert to nanoseconds
+        wakeup_ts = r[2] * 1000000  # Convert to nanoseconds
+        time_slept = (r[2] - r[0]) * 1000  # time slept in milliseconds
+        row = {
+                "timestamp": row_ts,
+                "fields" : {
+                    "time_slept" : time_slept
+                },
+                "tags" : {
+                    "device" : devices[f"dev-{r[1]}"]
+                }
+        }
+        results.append(row)
+        if f"dev-{r[1]}" not in devices_observed or devices_observed[f"dev-{r[1]}"] < wakeup_ts:
+            devices_observed[f"dev-{r[1]}"] = wakeup_ts
+
+    print("Sleep session data points:", len(results))
+
+    # Get sleep stage data
+    print("Querying sleep stage data...")
+    data_query = ("SELECT TIMESTAMP, DEVICE_ID, STAGE FROM COLMI_SLEEP_STAGE_SAMPLE "
+        f"WHERE TIMESTAMP >= {query_start_bound} "
+        "AND DEVICE_ID IN (" + ",".join([k.split('-')[1] for k in devices.keys()]) + ") "
+        "ORDER BY TIMESTAMP ASC")
+
+    res = cur.execute(data_query)
+    for r in res.fetchall():
+        row_ts = r[0] * 1000000  # Convert to nanoseconds
+        row = {
+                "timestamp": row_ts,
+                "fields" : {
+                    "sleep_stage" : r[2]
+                },
+                "tags" : {
+                    "device" : devices[f"dev-{r[1]}"]
+                }
+        }
+        results.append(row)
+        if f"dev-{r[1]}" not in devices_observed or devices_observed[f"dev-{r[1]}"] < row_ts:
+            devices_observed[f"dev-{r[1]}"] = row_ts
+
+    print("Sleep stage data points:", len(results))
+
+    # Get HRV data
+    print("Querying HRV data...")
+    data_query = ("SELECT TIMESTAMP, DEVICE_ID, VALUE FROM COLMI_HRV_VALUE_SAMPLE "
+        f"WHERE TIMESTAMP >= {query_start_bound} "
+        "AND DEVICE_ID IN (" + ",".join([k.split('-')[1] for k in devices.keys()]) + ") "
+        "ORDER BY TIMESTAMP ASC")
+
+    res = cur.execute(data_query)
+    for r in res.fetchall():
+        row_ts = r[0] * 1000000  # Convert to nanoseconds
+        row = {
+                "timestamp": row_ts,
+                "fields" : {
+                    "hrv_value" : r[2]
+                },
+                "tags" : {
+                    "device" : devices[f"dev-{r[1]}"]
+                }
+        }
+        results.append(row)
+        if f"dev-{r[1]}" not in devices_observed or devices_observed[f"dev-{r[1]}"] < row_ts:
+            devices_observed[f"dev-{r[1]}"] = row_ts
+
+    print("HRV data points:", len(results))
+
+    # Get activity data
+    print("Querying activity data...")
+    data_query = ("SELECT TIMESTAMP, DEVICE_ID, STEPS FROM COLMI_ACTIVITY_SAMPLE "
+        f"WHERE TIMESTAMP >= {query_start_bound} "
+        "AND DEVICE_ID IN (" + ",".join([k.split('-')[1] for k in devices.keys()]) + ") "
+        "ORDER BY TIMESTAMP ASC")
+
+    res = cur.execute(data_query)
+    for r in res.fetchall():
+        row_ts = r[0] * 1000000  # Convert to nanoseconds
+        row = {
+                "timestamp": row_ts,
+                "fields" : {
+                    "activity" : r[2]
+                },
+                "tags" : {
+                    "device" : devices[f"dev-{r[1]}"]
+                }
+        }
+        results.append(row)
+        if f"dev-{r[1]}" not in devices_observed or devices_observed[f"dev-{r[1]}"] < row_ts:
+            devices_observed[f"dev-{r[1]}"] = row_ts
+
+    print("Activity data points:", len(results))
+
+    # Get heart rate data
+    print("Querying heart rate data...")
+    data_query = ("SELECT TIMESTAMP, DEVICE_ID, HEART_RATE FROM COLMI_HEART_RATE_SAMPLE "
+        f"WHERE TIMESTAMP >= {query_start_bound} "
+        "AND DEVICE_ID IN (" + ",".join([k.split('-')[1] for k in devices.keys()]) + ") "
+        "AND HEART_RATE > 0 AND HEART_RATE < 254 "
+        "ORDER BY TIMESTAMP ASC")
+
+    res = cur.execute(data_query)
+    for r in res.fetchall():
+        row_ts = r[0] * 1000000  # Convert to nanoseconds
+        row = {
+                "timestamp": row_ts,
+                "fields" : {
+                    "heart_rate" : r[2]
+                },
+                "tags" : {
+                    "device" : devices[f"dev-{r[1]}"],
+                    "sample_type" : "periodic_samples"
+                }
+        }
+        results.append(row)
+        if f"dev-{r[1]}" not in devices_observed or devices_observed[f"dev-{r[1]}"] < row_ts:
+            devices_observed[f"dev-{r[1]}"] = row_ts
+
+    print("Heart rate data points:", len(results))
+
+    # Create a field to record when we last synced
+    now = time.time_ns()
+    for device in devices_observed:
+        row_ts = devices_observed[device]
+        row_age = now - row_ts
+        row = {
+                "timestamp": now,
+                "fields" : {
+                    "last_seen" : row_ts,
+                    "last_seen_age" : row_age
+                },
+                "tags" : {
+                    "device" : devices[device],
+                    "sample_type" : "sync_check"
+                }
+        }
+        results.append(row)
+
+    return results
+
+def write_results(results):
+    '''Open a connection to InfluxDB and write the results'''
+    print("Connecting to InfluxDB...")
+    with InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG) as _client:
+        with _client.write_api(write_options=SYNCHRONOUS) as _write_client:
+            for row in results:
+                p = Point(INFLUXDB_MEASUREMENT)
+                for tag in row['tags']:
+                    p = p.tag(tag, row['tags'][tag])
+                    
+                for field in row['fields']:
+                    if row['fields'][field] == -1:
+                        continue
+                    p = p.field(field, row['fields'][field])
+                    
+                p = p.time(row['timestamp'])
+                try:
+                    _write_client.write(INFLUXDB_BUCKET, INFLUXDB_ORG, p)
+                    print(f"Successfully wrote point: {p}")
+                except Exception as e:
+                    print(f"Failed to write point: {p}, error: {e}")
+
+if __name__ == "__main__":
+    print("Starting script...")
+    if not INFLUXDB_URL:
+        print("Error: INFLUXDB_URL not set in environment")
+        sys.exit(1)
+
+    tempdir = fetch_database()
+    print(f"Fetched database to temporary directory: {tempdir}")
+    conn, cur = open_database(tempdir)
+    print("Opened database connection")
+
+    # Extract data from the DB
+    results = extract_data(cur)
+    if not results:
+        print("Data extraction failed")
+        sys.exit(1)
+
+    print(f"Extracted {len(results)} data points")
+
+    # Write out to InfluxDB
+    write_results(results)
+    
+    # Tidy up
+    conn.close()
+    print("Closed database connection")
+    if tempdir not in ["/", ""] and REMOVE_TEMP_DB == "Y":
+        shutil.rmtree(tempdir)
+        print(f"Removed temporary directory: {tempdir}")
+    elif REMOVE_TEMP_DB == "N":
+        print(f"Temporary directory retained: {tempdir}")
